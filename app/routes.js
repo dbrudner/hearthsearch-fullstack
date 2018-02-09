c157cb120f7b1a895abc7deabada2c20af5926a5
dbrudner <dbrudne1@gmail.com>
2018-02-09 00:21:53 -0500

    commit

--- app/routes.js
+++ app/routes.js
@@ -17,11 +17,6 @@ module.exports = function(app, passport) {
     // SIGNUP ==============================
     // =====================================
     // show the signup form
-    app.get('/signup', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
 
 
     // =====================================
